0698bd864a8d60a5f6da145d2993d63e8c8888ab
Vitor Kruel <vkruel.programador@gmail.com>
2023-07-10 15:43:27 -0300

    Removendo partes inúteis

--- Introduction/recipes/views.py
+++ Introduction/recipes/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 from django.shortcuts import render
@@ -6,11 +5,3 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'recipes/home.html', context={'nome': 'Vitor Kruel'})
-
-
-def sobre(request):
-    return render(request, 'recipes/sobre.html')
-
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
