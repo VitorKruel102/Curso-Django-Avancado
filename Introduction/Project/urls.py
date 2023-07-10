e421f6e8db9c176e0478d50d84ab7e17f7f6389d
Vitor Kruel <vkruel.programador@gmail.com>
2023-07-10 14:36:35 -0300

    Criando o primeiro aplicativo.

--- Introduction/Project/urls.py
+++ Introduction/Project/urls.py
@@ -15,16 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.http import HttpResponse
-from django.urls import path
-
-
-def my_view(request):
-    return HttpResponse('Hello World!')
+from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', my_view),
-    path('sobre/', my_view),
+    path('', include('recipes.urls')), # dominino/...
+    # path('recipes/', include('recipes.urls')), # dominino/recipes/...
 ]
