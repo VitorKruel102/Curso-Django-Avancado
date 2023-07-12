aad048852275815a296541daf306ed68088eae9b
Vitor Kruel <vkruel.programador@gmail.com>
2023-07-12 15:42:03 -0300

    Entendendo sobre URLConfig.

--- Introduction/recipes/views.py
+++ Introduction/recipes/views.py
@@ -5,3 +5,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'recipes/pages/home.html', context={'nome': 'Vitor Kruel'})
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html', context={'id': id})
