79df220afc2f817a68979fb0e6a51d23af19b1a3
Vitor Kruel <vkruel.programador@gmail.com>
2023-07-17 15:07:17 -0300

    up.

--- Introduction/Project/urls.py
+++ Introduction/Project/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('recipes.urls')), # dominino/...
     # path('recipes/', include('recipes.urls')), # dominino/recipes/...
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+
+
--- Introduction/recipes/models.py
+++ Introduction/recipes/models.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-class Categoty(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=65)
 
+    def __str__(self):
+        return self.name
+    
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
@@ -19,6 +22,6 @@ class Recipe(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
-    category = models.ForeignKey(Categoty, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
