d155a82cba26a282bcd12ca64b6ab9ce63b6e332
Vitor Kruel <vkruel.programador@gmail.com>
2023-07-19 11:14:56 -0300

    Utilizando o get_list_or_404 do Djando para retornar o objeto em formato de lista ou dar o erro 404.

--- Introduction/recipes/views.py
+++ Introduction/recipes/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-from utils.recipes.factory import make_recipe
 from django.http import Http404
+from django.shortcuts import render, get_list_or_404
+from utils.recipes.factory import make_recipe
 
 from recipes.models import Recipe
 
@@ -16,17 +16,19 @@ def home(request):
     )
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
-
-    if not recipes:
-        raise Http404('Not Found!')
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id, 
+            is_published=True
+        ).order_by('-id')
+    )
 
     return render(
         request, 
         'recipes/pages/category.html', 
         context={
             'recipes': recipes,
-            'title_category': f'{recipes.first().category.name} - Category |',
+            'title_category': f'{recipes[0].category.name} - Category |',
         }
     )
 
