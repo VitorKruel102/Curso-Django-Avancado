87122a7850acc1acca597b8efd4661abc328626c
Vitor Kruel <vkruel.programador@gmail.com>
2023-07-19 11:02:01 -0300

    Adicionando o Not Found quando não tiver receitas.

--- Introduction/recipes/views.py
+++ Introduction/recipes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
+from django.http import Http404
 
 from recipes.models import Recipe
 
@@ -16,6 +17,10 @@ def home(request):
 
 def category(request, category_id):
     recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+
+    if not recipes:
+        raise Http404('Not Found!')
+
     return render(
         request, 
         'recipes/pages/category.html', 
