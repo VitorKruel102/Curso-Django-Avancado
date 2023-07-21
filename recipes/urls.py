ff530827fe5ecb82c7ddcfc0fb7ea796bbce76eb
Vitor Kruel <vkruel.programador@gmail.com>
2023-07-21 13:42:28 -0300

    Usando o TDD para criar a URL.

--- recipes/urls.py
+++ recipes/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.home, name='home'), # Home
     path('recipes/category/<int:category_id>/', views.category, name='category'), # Recipe
     path('recipes/<int:id>/', views.recipe, name='recipe'), # Recipe
+    path('recipes/search/', lambda request: ... , name='search'),
 ]
