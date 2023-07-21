00b2b2a8fb9e130de785ce849cca3cbd352e5020
Vitor Kruel <vkruel.programador@gmail.com>
2023-07-21 10:07:36 -0300

    Criando uma função genérica de make_recipe_no_defaults

--- recipes/tests/test_recipes_model_recipe.py
+++ recipes/tests/test_recipes_model_recipe.py
@@ -1,4 +1,4 @@
-from .test_recipes_base import RecipeTestBase
+from .test_recipes_base import RecipeTestBase, Recipe
 
 from django.core.exceptions import ValidationError
 from parameterized import parameterized
@@ -8,6 +8,22 @@ class RecipeModelTest(RecipeTestBase):
         """Inicializa sempre antes do teste."""
         self.recipe = self.make_recipe()
         return super().setUp
+
+    def make_recipe_no_defaults(self):
+        recipe = Recipe(
+            category=self.make_category(name='Test Default Category'),
+            author=self.make_author(username='newuser'),
+            title='Recipe Title',
+            description='Recipe Description',
+            slug='recipe-slug',
+            preparation_time=10,
+            preparation_time_unit='Minutos',
+            servings=5,
+            servings_unit='Porções',
+            preparation_step='Recipe Preparation Steps',
+        )
+        recipe.full_clean()
+        recipe.save()
 
     @parameterized.expand([
             ('title', 65),
@@ -20,3 +36,10 @@ class RecipeModelTest(RecipeTestBase):
 
         with self.assertRaises(ValidationError):
             self.recipe.full_clean() # AQUI A VALIDAÇÃO OCORRE
+
+    def test_recipe_preparation_step_is_html_is_false_by_default(self):
+        recipe = self.make_recipe_no_defaults()
+        self.assertFalse(
+            recipe.preparation_step_is_html, 
+            msg='Recipe preparation_step_is_html is not False'
+        )
