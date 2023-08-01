3b8a2feb2f160f5909694f04b30a54fa20dbea08
Vitor Kruel <vkruel.programador@gmail.com>
2023-08-01 12:44:58 -0300

    Sobrescrevendo campos do formulário.

--- authors/forms.py
+++ authors/forms.py
@@ -2,8 +2,49 @@ from django import forms
 from django.contrib.auth.models import User
 
 
+def add_placeholder(field, placeholder_val):
+    field.widget.attrs['placeholder'] = placeholder_val
+
+
 class RegisterForm(forms.ModelForm):
 
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        add_placeholder(self.fields['username'], 'Your username')
+        add_placeholder(self.fields['email'], 'Your e-mail')
+        add_placeholder(self.fields['first_name'], 'Your firts name')
+        add_placeholder(self.fields['last_name'], 'Your last name')
+
+
+    password = forms.CharField(
+        required=True, 
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': 'Your password',
+            }
+        ),
+        error_messages={
+            'required': 'Password must not be empty'
+        },
+        help_text=(
+            'Password must have at least one uppercase letter, '
+            'one lowercase letter and one number. The length should be' 
+            'at least 8 characters.'
+        )
+    )
+    password2 = forms.CharField(
+        required=True, 
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': 'Repeat your password',
+            }
+        ),
+        error_messages={
+            'required': 'Password must not be empty'
+        }
+    )
 
     class Meta:
         model = User
@@ -32,13 +73,4 @@ class RegisterForm(forms.ModelForm):
                 'max_length': 'This field must have less than 3 character',
                 'invalid': 'This fieldis invalid',
             }
-        }
-
-        widgets = {
-            'first_name': forms.TextInput(attrs={
-                'placeholder': 'Type your username here'
-            }),
-            'password': forms.PasswordInput(attrs={
-                'placeholder': 'Type yout passaword here.'
-            })
         }
