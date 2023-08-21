498eb5f7859b5711eb87c9104d28a705470dfb39
Vitor Kruel <vkruel.programador@gmail.com>
2023-08-21 10:22:04 -0300

    up.

--- tests/funcional_tests/recipes/test_recipe_home_page.py
+++ tests/funcional_tests/recipes/test_recipe_home_page.py
@@ -1,13 +1,15 @@
 from selenium.webdriver.common.by import By
 from .base import RecipeBaseFunctionalTest
+from recipes.tests.test_recipes_base import RecipeMixin
+from unittest.mock import patch
 import pytest
 
+
 @pytest.mark.functional_test
-class RecipesHomePageFuncionalTest(RecipeBaseFunctionalTest):
+class RecipesHomePageFuncionalTest(RecipeBaseFunctionalTest, RecipeMixin):
 
-
+    @patch('recipes.views.PER_PAGE', new=2)
     def test_recipe_home_page_without_recipes_error_message(self):
         self.browser.get(self.live_server_url)
         body = self.browser.find_element(By.TAG_NAME, 'body')
-
         self.assertIn('No recipes found here', body.text)
