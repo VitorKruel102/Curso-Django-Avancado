b875ab026b72b19d627bd70fa9f1578dd4f0642b
Vitor Kruel <vkruel.programador@gmail.com>
2023-08-23 11:09:13 -0300

    Criando lista de receitas do usuário.

--- authors/views.py
+++ authors/views.py
@@ -3,6 +3,7 @@ from django.http import Http404
 from django.contrib import messages
 from .forms import RegisterForm, LoginForm
 from django.urls import reverse
+from recipes.models import Recipe
 
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
@@ -94,4 +95,14 @@ def logout_view(request):
 
 @login_required(login_url='authors:login', redirect_field_name='next')
 def dashboard(request):
-    return render(request, 'authors/pages/dashboard.html')
+    recipes = Recipe.objects.filter(
+        is_published=False,
+        author=request.user
+    )
+    return render(
+        request,
+        'authors/pages/dashboard.html',
+        context={
+            'recipes': recipes,
+        }
+    )
