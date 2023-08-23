83d229495b2709be3bb237bc2e1a7b51bd884ed4
Vitor Kruel <vkruel.programador@gmail.com>
2023-08-23 10:53:44 -0300

    Personalizando a administração do django.

--- recipes/admin.py
+++ recipes/admin.py
@@ -9,4 +9,10 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    ...
+    list_display = ('id', 'title', 'created_at', 'is_published', 'author')
+    list_display_links = ('title', 'created_at')
+    list_filter = ('category', 'author', 'is_published')
+    search_fields = ('id', 'title', 'descriptions', 'slug', 'preparations_steps')
+    list_per_page = 10
+    list_editable = ['is_published']
+    ordering = ['id']
