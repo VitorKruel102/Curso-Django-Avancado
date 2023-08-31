b632875f6c62ab21064e707d415de670ba8c2689
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-08-31 13:44:57 -0300

    ajustando a grid do formulario

--- utils/django_forms.py
+++ utils/django_forms.py
@@ -5,6 +5,10 @@ import re
 def add_placeholder(field, placeholder_val):
     field.widget.attrs['placeholder'] = placeholder_val
 
+def add_attr(field, attr_name, attr_new_val):
+    existing = field.widget.attrs.get(attr_name, '')
+    field.widget.attrs[attr_name] = f'{existing} {attr_new_val}'.strip()
+
 def strong_password(password):
     regex = re.compile(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$')
     
