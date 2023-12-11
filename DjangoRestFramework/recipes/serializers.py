cfdcdb295e362d87da46ba57e02a12cfb38f9032
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-12-11 12:36:14 -0300

    UP.

--- DjangoRestFramework/recipes/serializers.py
+++ DjangoRestFramework/recipes/serializers.py
@@ -1,14 +1,53 @@
+from pyexpat import model
+from attr import field
 from rest_framework import serializers
+from tag.models import Tag
+
+from .models import Category, Recipe
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ['id', 'name', 'slug']
+
+    # id = serializers.IntegerField()
+    # name = serializers.CharField(max_length=255)
+    # slug = serializers.SlugField()
 
 
 class RecipeSerializer(serializers.Serializer):
-    ...
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=65)
-    description = serializers.CharField(max_length=165)
-    public = serializers.BooleanField(source='is_published')
-    preparation = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Recipe
+        fields = [
+            'id', 
+            'title',
+            'description',
+            'author',
+            'category',
+            'tags',
+            'public',
+            'preparation',
+            'tag_objects',
+            'tag_links',
+        ]
+
+    public = serializers.BooleanField(source='is_published', read_only=True)
+    preparation = serializers.SerializerMethodField(read_only=True)
+    category_name = serializers.StringRelatedField(source='category')
+    author = serializers.StringRelatedField()
+    tag_objects = TagSerializer(
+        many=True, 
+        source='tags', 
+        read_only=True,
+    )
+    tag_links = serializers.HyperlinkedRelatedField(
+        many=True,
+        source='tags',
+        view_name='recipes:recipes_api_v2_tag',
+        read_only=True,
+    )
 
     def get_preparation(self, recipe):
         return f'{recipe.preparation_time} {recipe.preparation_time_unit}'
-
--- DjangoRestFramework/recipes/views/api.py
+++ DjangoRestFramework/recipes/views/api.py
@@ -1,15 +1,25 @@
-from recipes.serializers import RecipeSerializer
+from django.shortcuts import get_object_or_404
+from recipes.serializers import RecipeSerializer, TagSerializer
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from tag.models import Tag
 
 from ..models import Recipe
 
 
-@api_view()
+@api_view(http_method_names=['get', 'post'])
 def recipe_api_list(request) -> Response:
-    recipes = Recipe.objects.get_published()[:10]
-    serializer = RecipeSerializer(instance=recipes, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        recipes = Recipe.objects.get_published()[:10]
+        serializer = RecipeSerializer(
+            instance=recipes,
+            many=True,
+            context={'request': request}
+        )
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        return Response('POST', status=status.HTTP_201_CREATED)
 
 
 @api_view()
@@ -17,11 +27,29 @@ def recipe_api_detail(request, pk) -> Response:
     recipe = Recipe.objects.filter(pk=pk).first()
 
     if recipe:
-        serializer = RecipeSerializer(instance=recipe, many=False)
+        serializer = RecipeSerializer(
+            instance=recipe,
+            many=False,
+            context={'request': request},
+        )
         return Response(serializer.data)
     return Response(
         {
-          'detail': 'Não Encontrado'
-        }, 
+            'detail': 'Não Encontrado'
+        },
         status=418
     )
+
+
+@api_view()
+def tag_api_detail(request, pk):
+    tag = get_object_or_404(
+        Tag.objects.all(),
+        pk=pk
+    )
+    serializer = TagSerializer(
+        instance=tag,
+        many=False,
+        context={'request': request},
+    )
+    return Response(serializer.data)
