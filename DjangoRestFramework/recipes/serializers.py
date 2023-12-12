d36e22ae4c9e7ecb08844a48e4d1329d09f3c4d5
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-12-12 10:03:46 -0300

    up.

--- DjangoRestFramework/recipes/serializers.py
+++ DjangoRestFramework/recipes/serializers.py
@@ -1,9 +1,11 @@
-from pyexpat import model
-from attr import field
+from collections import defaultdict
+
+from attr import attr
+from authors.validators import AuthorRecipeValidator
 from rest_framework import serializers
 from tag.models import Tag
 
-from .models import Category, Recipe
+from .models import Recipe
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -11,35 +13,32 @@ class TagSerializer(serializers.ModelSerializer):
         model = Tag
         fields = ['id', 'name', 'slug']
 
-    # id = serializers.IntegerField()
-    # name = serializers.CharField(max_length=255)
-    # slug = serializers.SlugField()
 
-
-class RecipeSerializer(serializers.Serializer):
-    
+class RecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipe
         fields = [
-            'id', 
-            'title',
-            'description',
-            'author',
-            'category',
-            'tags',
-            'public',
-            'preparation',
-            'tag_objects',
-            'tag_links',
+            'id', 'title', 'description', 'author',
+            'category', 'tags', 'public', 'preparation',
+            'tag_objects', 'tag_links', 
+            'preparation_time', 'preparation_time_unit', 'servings',
+            'servings_unit',
+            'preparation_steps', 'cover'
         ]
 
-    public = serializers.BooleanField(source='is_published', read_only=True)
-    preparation = serializers.SerializerMethodField(read_only=True)
-    category_name = serializers.StringRelatedField(source='category')
-    author = serializers.StringRelatedField()
+    public = serializers.BooleanField(
+        source='is_published',
+        read_only=True,
+    )
+    preparation = serializers.SerializerMethodField(
+        method_name='any_method_name',
+        read_only=True,
+    )
+    category = serializers.StringRelatedField(
+        read_only=True,
+    )
     tag_objects = TagSerializer(
-        many=True, 
-        source='tags', 
+        many=True, source='tags',
         read_only=True,
     )
     tag_links = serializers.HyperlinkedRelatedField(
@@ -49,5 +48,29 @@ class RecipeSerializer(serializers.Serializer):
         read_only=True,
     )
 
-    def get_preparation(self, recipe):
+    def any_method_name(self, recipe):
         return f'{recipe.preparation_time} {recipe.preparation_time_unit}'
+
+    def validate(self, attrs):
+        if self.instance is not None and attrs.get('servings') is None:
+            attrs['servings'] = self.instance.servings
+
+        if self.instance is not None and attrs.get('preparation_time') is None:
+            attrs['preparation_time'] = self.instance.preparation_time
+
+        super_validate = super().validate(attrs)
+        AuthorRecipeValidator(
+            data=attrs,
+            ErrorClass=serializers.ValidationError,
+        )
+        return super_validate
+
+
+    def save(self, **kwargs):
+        return super().save(**kwargs)
+
+    def create(self, validated_data):
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        return super().update(instance, validated_data)
--- DjangoRestFramework/recipes/views/api.py
+++ DjangoRestFramework/recipes/views/api.py
@@ -1,44 +1,65 @@
 from django.shortcuts import get_object_or_404
-from recipes.serializers import RecipeSerializer, TagSerializer
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from tag.models import Tag
 
 from ..models import Recipe
+from ..serializers import RecipeSerializer, TagSerializer
 
 
 @api_view(http_method_names=['get', 'post'])
-def recipe_api_list(request) -> Response:
+def recipe_api_list(request):
     if request.method == 'GET':
         recipes = Recipe.objects.get_published()[:10]
         serializer = RecipeSerializer(
             instance=recipes,
             many=True,
-            context={'request': request}
+            context={'request': request},
         )
         return Response(serializer.data)
     elif request.method == 'POST':
-        return Response('POST', status=status.HTTP_201_CREATED)
+        serializer = RecipeSerializer(
+            data=request.data,
+            context={'request': request},
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED
+        )
 
 
-@api_view()
-def recipe_api_detail(request, pk) -> Response:
-    recipe = Recipe.objects.filter(pk=pk).first()
-
-    if recipe:
+@api_view(['get', 'patch', 'delete'])
+def recipe_api_detail(request, pk):
+    recipe = get_object_or_404(
+        Recipe.objects.get_published(),
+        pk=pk
+    )
+    if request.method == 'GET':
         serializer = RecipeSerializer(
             instance=recipe,
             many=False,
             context={'request': request},
         )
         return Response(serializer.data)
-    return Response(
-        {
-            'detail': 'Não Encontrado'
-        },
-        status=418
-    )
+    elif request.method == 'PATCH':
+        serializer = RecipeSerializer(
+            instance=recipe,
+            data=request.data,
+            many=False,
+            context={'request': request},
+            partial=True,
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(
+            serializer.data,
+        )
+    elif request.method == 'DELETE':
+        recipe.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view()
